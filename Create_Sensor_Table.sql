5d247a763d175bd42a3ebb13a27c2b8de2c63d79
Lammert Bakker <r0795383@student.vives.be>
2020-03-26 15:26:39 +0100

    Created databases for Users and Sensors

---
+++ Create_Sensor_Table.sql
@@ -0,0 +1,7 @@
+﻿Create TABLE Sensors (
+	SensorId int IDENTITY(1,1) PRIMARY KEY NOT NULL ,
+	UserId int references Users(UserId),
+	Latitude float NOT NULL,
+	Longitude float NOT NULL,
+	SensorType varchar
+);
---
+++ Create_User_Table.sql
@@ -0,0 +1,6 @@
+﻿Create TABLE Users (
+	UserId int IDENTITY(1,1) PRIMARY KEY NOT NULL ,
+	UserName varchar NOT NULL,
+	UserPassword varchar NOT NULL,
+	Email varchar
+);
