881a9ba47402a05d35d6f3a52db91946cf113a93
Lammert Bakker <r0795383@student.vives.be>
2020-04-02 23:15:16 +0200

    Referencing primary key in model

--- DevbitApi/Models/UserModel.cs
+++ DevbitApi/Models/UserModel.cs
@@ -9,7 +9,9 @@ namespace DevbitApi.Models
 {
     public class UserModel
     {
+        [Key]
         public int UserId { get; set; }
+
         public string UserName { get; set; }
         public string UserPassword { get; set; }
         public string Email { get; set; }
